4f6459c5a1efab6d12add5060a503d9255e2ff8a
Nicole Mejia <snicolemejia@gmail.com>
2023-02-10 10:42:45 -0500

    starts code for FriendList

---
+++ components/FriendList.jsx
@@ -0,0 +1,11 @@
+import UserCard from './UserCard';
+import { Container, Text } from '@nextui-org/react';
+
+export default function FriendList({ friends }) {
+  return (
+    <Container>
+      <Text>Friend List</Text>
+      
+    </Container>
+  );
+}
